0e3c211f55e1d488322c633d5dc3e476d2ec3904
Sergio Peña <justsergioit@gmail.com>
2024-12-17 17:54:32 +0100

    main: Removed neon usage

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,6 @@
 import "dotenv/config";
-import { drizzle as devDB } from "drizzle-orm/postgres-js";
-import { drizzle as prodDB } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { neon } from "@neondatabase/serverless";
-
 import * as orgs from "./schema/orgs";
 import * as userSettings from "./schema/user-settings";
 import * as userTokens from "./schema/user-tokens";
@@ -11,26 +8,15 @@ import * as users from "./schema/users";
 import * as auditLogs from "./schema/audit-logs";
 
 const connectionString = process.env.DATABASE_URL!;
+export const client = postgres(connectionString, { prepare: false });
 
-export const db =
-  process.env.NODE_ENV === "production"
-    ? prodDB({
-        client: neon(connectionString),
-        schema: {
-          ...orgs,
-          ...userSettings,
-          ...userTokens,
-          ...users,
-          ...auditLogs,
-        },
-      })
-    : devDB({
-        client: postgres(connectionString),
-        schema: {
-          ...orgs,
-          ...userSettings,
-          ...userTokens,
-          ...users,
-          ...auditLogs,
-        },
-      });
+export const db = drizzle({
+  client,
+  schema: {
+    ...orgs,
+    ...userSettings,
+    ...userTokens,
+    ...users,
+    ...auditLogs,
+  },
+});
