9b6166b69c54e61720190cab52aec23c9fc51cba
Sergio Peña <justsergioit@gmail.com>
2025-02-14 14:51:17 +0100

    feat(ci): Added docker-compose and updated drizzle out, migrations are not applied
    feat(ci): Added dockerfile but port is not configurable

--- apps/loki/drizzle.config.ts
+++ apps/loki/drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./supabase/migrations",
+  out: "./drizzle",
   schema: "./db/schema",
   dialect: "postgresql",
   dbCredentials: {
